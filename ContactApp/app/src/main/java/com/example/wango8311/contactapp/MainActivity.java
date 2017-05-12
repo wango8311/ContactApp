e6d40e9adf25d1656dbdacc68ef1d483eb30c1fa
wango8311 <orson2000123456789@gmail.com>
2017-05-12 10:38:18 -0700

    Created database. Added Name field. Added button to add record to the database.

--- ContactApp/app/src/main/java/com/example/wango8311/contactapp/MainActivity.java
+++ ContactApp/app/src/main/java/com/example/wango8311/contactapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.wango8311.contactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText editName;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
     }
 }
